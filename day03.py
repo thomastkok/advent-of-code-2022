12c660c02ab683760c817ddd44432cb34389ca65
Thomas Kok <Thomas.Kok@UGent.be>
2022-12-03 21:52:42 +0100

    Reformat line length

--- day03.py
+++ day03.py
@@ -6,4 +6,9 @@ def part_one(rucksacks):
 
 
 def part_two(rucksacks):
-    sum(map(lambda x: ord(x) - 38 if x.isupper() else ord(x) - 96, [reduce(lambda a, b: a & b, map(set, [rucksacks.split("\n")[i + j] for j in range(3)])).pop() for i in range(0, len(rucksacks.split("\n")) - 2, 3)]))
+    sum(
+        map(
+            lambda x: ord(x) - 38 if x.isupper() else ord(x) - 96,
+            [reduce(lambda a, b: a & b, map(set, [rucksacks.split("\n")[i + j] for j in range(3)])).pop() for i in range(0, len(rucksacks.split("\n")) - 2, 3)],
+        )
+    )
