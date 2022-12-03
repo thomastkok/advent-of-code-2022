bedbc517bd4f0dc7afa8bbfc9b9ae2c45f31a613
Thomas Kok <Thomas.Kok@UGent.be>
2022-12-03 21:50:45 +0100

    Solve day 03

---
+++ day03.py
@@ -0,0 +1,9 @@
+from functools import reduce
+
+
+def part_one(rucksacks):
+    sum(map(lambda x: ord(x) - 38 if x.isupper() else ord(x) - 96, [(set(y[: len(y) // 2]) & set(y[len(y) // 2 :])).pop() for y in rucksacks.split("\n")]))
+
+
+def part_two(rucksacks):
+    sum(map(lambda x: ord(x) - 38 if x.isupper() else ord(x) - 96, [reduce(lambda a, b: a & b, map(set, [rucksacks.split("\n")[i + j] for j in range(3)])).pop() for i in range(0, len(rucksacks.split("\n")) - 2, 3)]))
